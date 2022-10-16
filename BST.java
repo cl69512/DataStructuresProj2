bba1161fdcbd34123c17f95806c5c8d44008a367
Gavin Gaude <gwg08270@uga.edu>
2022-10-15 20:59:00 -0400

    Implemented inorder() and insert()

--- BST.java
+++ BST.java
@@ -1,117 +1,192 @@
 import java.lang.Math;
 import java.util.*;
 
-public class BST { 
-  
-  private class Node { 
-  
-    int key;
-    Node parent;
-    Node leftChild;
-    Node rightChild;
-  
-    private Node() { 
+public class BST {
+
+  static class Node {
+
+    private int key;
+    private Node parent;
+    private Node leftChild;
+    private Node rightChild;
+
+    private Node() {
       this.key = -1;
       this.parent = null;
-      this.leftChild = null; 
+      this.leftChild = null;
       this.rightChild = null;
     } //Node()
-    
-    private Node (int key) { 
+
+    private Node (int key) {
       this();
       this.key = key;
     }
-    
-    private Node(int key, Node parent, Node leftChild, Node rightChild) { 
+
+    private Node(int key, Node parent, Node leftChild, Node rightChild) {
       this.key = key;
       this.parent = parent;
       this.leftChild = leftChild;
       this.rightChild = rightChild;
     }
-    
-    public void setKey( int key) { 
+
+    public void setKey( int key) {
       this.key = key;
-    } 
-    
-    public void getKey() { 
+    }
+
+    public int getKey() {
       return this.key;
     }
-    
-    public void setParent(Node parent) { 
+
+    public void setParent(Node parent) {
       this.parent = parent;
-    } 
-    
-    public void getParent() { 
-      return this.Parent;
     }
-    
-    public void setleftChild(Node leftChild) { 
+
+    public Node getParent() {
+      return this.parent;
+    }
+
+    public void setLeftChild(Node leftChild) {
       this.leftChild = leftChild;
-    } 
-    
-    public void getleftChild() { 
+    }
+
+    public Node getLeftChild() {
       return this.leftChild;
     }
-    
-    public void setrightChild(Node rightChild) { 
+
+    public void setRightChild(Node rightChild) {
       this.rightChild = rightChild;
-    } 
-    
-    public void getrightChild() { 
+    }
+
+    public Node getRightChild() {
       return this.rightChild;
     }
-  
-  } // Node 
-  
-  Node root; 
-  int counter;
-  
-  public BST() { 
+
+  } // Node
+
+  private Node root;
+  private int counter;
+
+  public BST() {
     this.root = null;
     this.counter = 0;
   } //BST()
-  
-  /*
+
+  /**
   * checks to see if the left and right children are null
   * increments the size if the children are not null
   * @return size
   */
-  public int size() { 
-    int size = 1;
-    
-    if (leftChild != null) { 
-      size +=leftChild.size();      
-    } // if 
-    
-    if (rightChild != null) { 
-      size += rightChild.size()
-    } // if 
-    return size;
-  } //size 
-  
-  public void insert(int element) { 
-  } // insert 
-  
-  public void delete(int element) { 
+  public int size() {
+      return this.counter;
+  } //size
+
+  /**
+   * Adds element to the tree according to binary search tree structure
+   * @param element integer added to tree
+   */
+  public void insert(int element) {
+      // If tree is empty, makes the element the root
+      if (this.size() == 0) {
+          this.root = new Node(element);
+          this.counter++;
+          return;
+      } // if
+
+      Node temp = this.root;
+
+      // Loops until element is placed in tree
+      while (temp != null) {
+          // Checks if element is already in tree
+          if (element == temp.getKey()) {
+              System.out.println("Element is already in the tree!");
+              return;
+          } // if
+          // Moves left on tree
+          if (element < temp.getKey()) {
+              if (temp.getLeftChild() != null) {
+                  temp = temp.getLeftChild();
+              } else {
+                  Node newNode = new Node(element);
+                  temp.setLeftChild(newNode);
+                  newNode.setParent(temp);
+                  this.counter++;
+                  return;
+              } // if
+            // Moves right on tree
+          } else {
+              if (temp.getRightChild() != null) {
+                  temp = temp.getRightChild();
+              } else {
+                  Node newNode = new Node(element);
+                  temp.setRightChild(newNode);
+                  newNode.setParent(temp);
+                  this.counter++;
+                  return;
+              } // if
+          } // if
+      } // while
+
+  } // insert
+
+
+
+    /*
+
+  public void delete(int element) {
   } // delete
-  
-  public void preorder() { 
+
+  public void preorder() {
   } // preorder
-  
-  public void postorder() { 
-  } //postorder 
-  
-  public void inorder() { 
+
+  public void postorder() {
+  } //postorder
+
+    */
+
+    /**
+     * Prints the elements of the tree inorder, meaning checking
+     * the left subtree, the node, and the right subtree
+     */
+  public void inorder() {
+      if (this.root == null) {
+          return;
+      } // if
+      inorderHelper(root);
   } //inorder
-  
-  private preorderHelper(Node current) {
+
+    /*
+
+  private void preorderHelper(Node current) {
   } //preorderHelper
-  
-  private postorderHelper(Node current) { 
+
+  private void postorderHelper(Node current) {
   } //postorderHelper
-  
-  private inorderHelper (Node current) { 
+
+    */
+
+    /**
+     * Helper method for inorder that recursively checks left
+     * and right nodes for order
+     * @param current node being checked
+     */
+  private void inorderHelper (Node current) {
+      if (current.getLeftChild() != null) {
+          inorderHelper(current.getLeftChild());
+      } // if
+      System.out.println(current.getKey());
+      if (current.getRightChild() != null) {
+          inorderHelper(current.getRightChild());
+      } // if
   } //inorderHelper
 
+   public static void main(String[] args) {
+       BST tree = new BST();
+       tree.insert(12);
+       tree.insert(3);
+       tree.insert(13);
+       tree.insert(6);
+       tree.insert(14);
+       tree.inorder();
+   } // main
 
-} // BST 
+} // BST
